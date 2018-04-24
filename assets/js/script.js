7c123136384eb9e75c003c3e456e6e757d129ceb
Michael Lum <michaellum@airbears2-10-142-84-213.airbears2.1918.berkeley.edu>
2018-04-24 13:21:56 -0700

    added jquery functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-
 	$(".button, #name").click(function() {
-		$("#name").css({"text-decoration": "none"});
+		$("#name").css({"text-decoration": "none"}); //jQuery Function #1
 		$(".button").css({"text-decoration": "none"});
 		$(this).css({"text-decoration": "underline", "text-decoration-color": "#FCBF49"});
 	});
 
+	$("#name").click(function() {
+		$(window).scrollTop(0); //jQuery Function #2
+	})
 });
