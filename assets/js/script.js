26c9955f8d9a1ac9d967746adda10f912cd581ae
Michael Lum <michaellum@Michaels-MacBook-Pro-8.local>
2018-04-24 19:43:45 -0700

    added links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-	$(".button, #name").click(function() {
-		$("#name").css({"text-decoration": "none"}); //jQuery Function #1
-		$(".button").css({"text-decoration": "none"});
-		$(this).css({"text-decoration": "underline", "text-decoration-color": "#FCBF49"});
+	$(".button").click(function() { //jQuery Function #1
+		$("#name").css({"text-decoration": "none"}); //jQuery Function #2
+
+		$(".button").removeClass("highlighted"); //jQuery Function #3
+		$(this).addClass("highlighted"); //jQuery Function #4
 	});
 
 	$("#name").click(function() {
-		$(window).scrollTop(0); //jQuery Function #2
+		$("#name").css({"text-decoration": "none"});
+		$(".button").removeClass("highlighted");
+		$("#name").css({"text-decoration": "underline", "text-decoration-color":"#FCBF49"});
+	});
+
+	$("#name").click(function() {
+		$(window).scrollTop(0); //jQuery Function #5
 	})
+
+	$(".logo").hover(function() { //jQuery Function #6
+		$(this).animate({"opacity":"1.0"}, 200);
+	});
 });
